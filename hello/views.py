fb74d85aa348cb3d751774a53011180e7b4481c5
LukkyDuck <77422744+LukkyDuck@users.noreply.github.com>
2021-11-01 22:28:19 -0300

    Initial commit

---
+++ hello/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+ # Create your views here.
+# Note: The stuff above this will already be in this file.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("AAAAAAAAAAAH TE ODIO FELIPE RITZ")
